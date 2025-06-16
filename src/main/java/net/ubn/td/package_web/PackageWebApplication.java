d8d55927ad5bb283b698e82668ba49c37771b9f4
CSLin <cs.lin@ubn.net>
2025-06-16 09:44:34 +0800

    第一次放上

---
+++ src/main/java/net/ubn/td/package_web/PackageWebApplication.java
@@ -0,0 +1,13 @@
+package net.ubn.td.package_web;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PackageWebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PackageWebApplication.class, args);
+	}
+
+}
---
+++ src/test/java/net/ubn/td/package_web/PackageWebApplicationTests.java
@@ -0,0 +1,13 @@
+package net.ubn.td.package_web;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PackageWebApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
